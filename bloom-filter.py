24bcd48209750c93b205838e8fd8a8cf05627215
Livia Leite <livialeites1@gmail.com>
2023-11-16 16:05:05 -0300

    Update bloom-filter.py

--- bloom-filter.py
+++ bloom-filter.py
@@ -10,6 +10,7 @@ class ArrayOfZeros:
             self.data.append(0)
 
 class BloomFilter:
+    
     def __init__(self, array, hashFunctions):
         self.array = array
         self.hashFunctions = hashFunctions
@@ -18,7 +19,6 @@ class BloomFilter:
         for hashFunction in self.hashFunctions:
             index = hashFunction(element) % self.array.size
             self.array.data[index] = 1
-            print('index add:',index)
 
     def search(self, element):
         for hashFunction in self.hashFunctions:
@@ -27,24 +27,19 @@ class BloomFilter:
             if self.array.data[index] != 1:
                 print('não pertence')
                 return False
-        print('pertence')
         return True
     
 
 def hashFunction1(element):
     hash = 0
-
     for index in range(10):
         hash = hash * 7 + ord(element[index])
-    # print(hash)
     return hash
 
 
-bloomfilter = BloomFilter(ArrayOfZeros(100), [hashFunction1])
+bloomFilterSpam = BloomFilter(ArrayOfZeros(100), [hashFunction1])
 
-emailSpam = 'emailspam.com.br'
-email = 'tesfhsodif.com.br'
+bloomFilterSpam.add()
+bloomFilterSpam.search()
 
-bloomfilter.add(emailSpam)
-bloomfilter.search(emailSpam)
-bloomfilter.search(email)
+
